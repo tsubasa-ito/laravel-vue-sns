e9e103aa70cb1b63e086ae4bf73b47a5ae94ea92
伊藤翼 <tsubasa8770@gmail.com>
2021-02-08 15:24:22 +0900

    4-3 記事投稿のビュー作成

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -17,4 +17,4 @@
 
 Auth::routes();
 Route::get('/', 'ArticleController@index')->name('articles.index');
-Route::resource('/articles', 'ArticleController')->except(['index']);
+Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth');
